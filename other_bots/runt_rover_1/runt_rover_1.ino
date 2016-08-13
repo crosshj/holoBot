9365d05ae0f05ce84b1fd21ffabe5b278079ef78
HJ Cross <github@chimpjuice.com>
2016-08-12 22:12:28 -0400

    both motors connected independently to motor controller

--- other_bots/runt_rover_1/runt_rover_1.ino
+++ other_bots/runt_rover_1/runt_rover_1.ino
@@ -1,14 +1,28 @@
-int IN1=8;
-int IN2=9;
-int ENA=10;
+int ENA=6;
+int ENB=9;
+int IN1=10;
+int IN2=11;
+int IN3=12;
+int IN4=13;
+
+
+int ASPEED = 100;
+int BSPEED = 98;
 
 void setup()
 {
  pinMode(IN1,OUTPUT);
  pinMode(IN2,OUTPUT);  
- analogWrite(ENA, 200);// motor speed  
+ pinMode(IN3,OUTPUT);
+ pinMode(IN4,OUTPUT);  
+
+ analogWrite(ENA, ASPEED);// motor speed  
  digitalWrite(IN1,LOW);// rotate forward
  digitalWrite(IN2,HIGH);
+
+ analogWrite(ENB, BSPEED);// motor speed  
+ digitalWrite(IN3,LOW);// rotate forward
+ digitalWrite(IN4,HIGH);
 
 }
 
